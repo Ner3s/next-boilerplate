c906f8f30a121489a1f35d401d5269548f19a1f3
Alan Neres <alanneres10@hotmail.com>
2022-07-12 20:09:20 -0300

    chore: add coverage from .ts files

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 };
 
