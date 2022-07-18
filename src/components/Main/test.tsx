973d8456f6197f0acd2a963ead6558ed80bb06ed
Alan Neres <alanneres10@hotmail.com>
2022-07-17 22:26:28 -0300

    fix: linting and add next in order imports

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-
 import { Main } from '.';
+
+import { render, screen } from '@testing-library/react';
 
 describe('<Main />', () => {
   it('should render the heading', () => {
